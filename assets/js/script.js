b3c64a171eadfa64119c69d5daf50c12efd61f3c
Samuel Näsström <nasstromsamuel@icloud.com>
2022-09-21 11:13:15 +0000

    Add startpage and game page.

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+const question = document.getElementById("question");
+const answers = Array.from(document.getElementsByClassName("button"));
+
+for (let answers of answers) {
+    answers.addEventListener("click", function() {
+
+    })
+}
+
+
+function newQuestion() {
+    
+}
+
+function checkAnswer() {
+    
+}
+
+function incrementScore() {
+    
+}
+
+function incrementWrongScore() {
+    
+}
