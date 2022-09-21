644387415fa350756cc5d4c73d92c645e75ea01a
Samuel Näsström <nasstromsamuel@icloud.com>
2022-09-21 11:56:38 +0000

    Added questions to a const array in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,83 @@
-const question = document.getElementById("question");
-const answers = Array.from(document.getElementsByClassName("button"));
+// constants for elements
+const gameBox = document.getElementById("game");
 
-for (let answers of answers) {
-    answers.addEventListener("click", function() {
-
-    })
-}
-
+// This Array contains the questions and answers for the game.
+const questions = [
+    {
+        question: "What is the Capital of Indonesia?",
+        answers: [
+            {text: "Jakarta", correct: true},
+            {text: "Beijing", correct: false},
+            {text: "Beirut", correct: false},
+            {text: "Baku", correct: false}
+        ],
+        question: "What is the Capital of Mongolia?",
+        answers: [
+            {text: "Seoul", correct: false},
+            {text: "Tehran", correct: false},
+            {text: "Ulaanbaatar", correct: true},
+            {text: "Malé", correct: false}
+        ],
+        question: "What is the Capital of Australia?",
+        answers: [
+            {text: "Sydney", correct: false},
+            {text: "Canberra", correct: true},
+            {text: "Melbourne", correct: false},
+            {text: "Bisbane", correct: false}
+        ],
+        question: "What is the Capital of Japan?",
+        answers: [
+            {text: "Sapporo", correct: false},
+            {text: "Kyoto", correct: false},
+            {text: "Osaka", correct: false},
+            {text: "Tokyo", correct: true}
+        ],
+        question: "What is the Capital of Slovakia?",
+        answers: [
+            {text: "Bratislava", correct: true},
+            {text: "Wien", correct: false},
+            {text: "München", correct: false},
+            {text: "Prag", correct: false}
+        ],
+        question: "What is the Capital of Egypt?",
+        answers: [
+            {text: "Luxor", correct: false},
+            {text: "Aswan", correct: false},
+            {text: "Cairo", correct: true},
+            {text: "Alexandria", correct: false}
+        ],
+        question: "What is the Capital of Kenya?",
+        answers: [
+            {text: "Nairobi", correct: true},
+            {text: "Mombasa", correct: false},
+            {text: "Kisumu", correct: false},
+            {text: "Nakuru", correct: false}
+        ],
+        question: "What is the Capital of Peru?",
+        answers: [
+            {text: "Piura", correct: false},
+            {text: "Lima", correct: true},
+            {text: "Arequipa", correct: false},
+            {text: "Santiago", correct: false}
+        ],
+        question: "What is the Capital of Colombia?",
+        answers: [
+            {text: "Cusco", correct: false},
+            {text: "Iquitos", correct: false},
+            {text: "Buenos Aires", correct: false},
+            {text: "Bogota", correct: true}
+        ],
+        question: "What is the Capital of Bulgaria?",
+        answers: [
+            {text: "Sofia", correct: true},
+            {text: "Bukarest", correct: false},
+            {text: "Budapest", correct: false},
+            {text: "Kiev", correct: false}
+        ]
+        
+    }
+]
+console.log(questions);
 
 function newQuestion() {
     
